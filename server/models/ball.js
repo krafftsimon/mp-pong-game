48dd3569fe416fb9e37797509ed5977f6fcfa391
simon <krafftsimon@gmail.com>
2017-10-07 18:45:33 -0400

    test

--- server/models/ball.js
+++ server/models/ball.js
@@ -23,8 +23,8 @@ class Ball {
     if (this.x < 5) {
       this.xSpeed = -this.xSpeed;
     }
-    if ((this.x < 40 && this.y > player1.y && this.y < player1.y + 100) ||
-        (this.x > 760 && this.y > player2.y && this.y < player2.y + 100)) {
+    if ((this.x <= 45 && this.y + 5 >= player1.y && this.y - 5 <= player1.y + 100) ||
+        (this.x >= 755 && this.y + 5 >= player2.y && this.y - 5 <= player2.y + 100)) {
       this.xSpeed = -this.xSpeed;
     }
   }

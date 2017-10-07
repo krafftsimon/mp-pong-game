d5f7df6f452e044d3afb0fff6e1634d48ddc71b4
simon <krafftsimon@gmail.com>
2017-10-07 13:35:58 -0400

    test

--- server.js
+++ server.js
@@ -94,7 +94,6 @@ function moveBall(i) {
 }
 
 function sendGameState() {
-  console.log(rooms[0].inputsP1);
   io.sockets.emit('gameState', rooms);
   //clear list of inputs received and ball steps for the next update
   for (let i in rooms) {
